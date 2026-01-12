f234bf6414e5696a1cbf65516c65a70993506cf5
RoadToRight <syedsameershah2008@gmail.com>
2026-01-12 04:51:45 -0800

    subscribe mail

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,25 +1,34 @@
 import nodemailer from 'nodemailer';
- 
-export const sendEmail = async({email,subject,message}) => {
-    let transporter = nodemailer.createTransport({
-  service: process.env.SMTP_SERVICE,
-  host:process.env.SMTP_HOST,
-  port:process.env.SMTP_PORT,
-  
-  auth: {
-    user: process.env.SMTP_MAIL,
-    pass: process.env.SMTP_PASSWORD
-  }
-});
 
-let mailOptions = {
-  from: process.env.SMTP_MAIL,
-  to: email,
-  subject: subject,
-  text: message
+export const sendEmail = async ({ email, subject, message, headers = false }) => {
+  let transporter = nodemailer.createTransport({
+    service: process.env.SMTP_SERVICE,
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD
+    }
+  });
+
+  let mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: email,
+    subject: subject,
+    text: message,
+    
+    ...(headers && {headers: {
+      'List-Unsubscribe': '<mailto:unsubscribe@example.com>, <https://example.com/unsubscribe>',
+        'List-Unsubscribe-Post': 'List-Unsubscribe=One-Click'
+    }})
+  
+
+
+
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+transporter.sendMail(mailOptions, function (error, info) {
   if (error) {
     console.log(error);
   } else {
